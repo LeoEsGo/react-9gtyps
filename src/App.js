37462248eff0f2dd76672f515e36193f8593a19d
LeoEsGo <81502454+LeoEsGo@users.noreply.github.com>
2022-10-28 00:13:42 -0500

    1 file modified, 2 added, 2 deleted

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
-import React from "react";
-import "./style.css";
-import Carro from "./Carro.js";
-import Moto from "./Moto.js";
+import React, { useState, useEffect } from 'react';
+import './style.css';
+import Carro from './Vehiculos/Carro.js';
 
 export default function App() {
+  const modelosDesdeAPI = [
+    {
+      nombre: 'nissan Gtr hakosuka',
+      modelo: 'Nissan C10 Skyline',
+      imagen:
+        'https://w7.pngwing.com/pngs/179/447/png-transparent-nissan-skyline-gt-r-nissan-gt-r-nissan-z-car-nissan-skyline-car-vehicle-nissan-thumbnail.png',
+      descripcion:
+        'La tercera generación del C10 Skyline, fue apodado «Hakosuka» (pronunciado Hak-OH-skaa y que los anglosajones denominan Boxy Skyline). ',
+      sonido: 'Ruuuuuuuuuuttuuuuuu',
+      anio: '2000',
+    },
+    {
+      nombre: 'harley davidson sportster 48 ',
+      modelo: 'sportster 48',
+      imagen:
+        'https://e7.pngegg.com/pngimages/54/360/png-clipart-harley-davidson-sportster-motorcycle-suspension-harley-davidson-evolution-engine-harley-motorcycle-custom-motorcycle.png',
+      descripcion:
+        'Harley-Davidson Forty-Eight es la que monta el propulsor de mayor cilindrada dentro de la gama Sportster. En esta ocasión, el motor V-Twin Air-Cooled Evolution ™ es de 1.202cc y se caracteriza por tener una gran par motor en bajas revoluciones: el par motor máximo que alcanza es de 96Nm a 3.500rpm.',
+      sonido: 'prum prum prum',
+      anio: '2022',
+    },
+  ];
+  const [modelos, setModelos] = useState([]);
+  useEffect(() => {
+    setModelos(modelosDesdeAPI);
+  }, []);
+
   return (
-    <div align="center">
-      <h2>Hola!</h2>
-       <Moto marca="Harley davidson Sportster 48" imagen="https://www.pngmart.com/files/10/Harley-Davidson-PNG-Transparent.png" descripcion="La nueva Harley-Davidson Forty-Eight es la que monta el propulsor de mayor cilindrada dentro de la gama Sportster. En esta ocasión, el motor V-Twin Air-Cooled Evolution ™ es de 1.202cc y se caracteriza por tener una gran par motor en bajas revoluciones: el par motor máximo que alcanza es de 96Nm a 3.500rpm."/> 
-       <Carro marca="BMW M3" descripcion="Soy un m3 de 8 cilindros" imagen="https://img.lovepik.com/free-png/20220120/lovepik-a-car-png-image_401521869_wh860.png"/>
+    <div
+      style={{
+        display: 'flex',
+        flexFlow: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      {modelos.map((modelos) => (
+        <Carro
+          nombre={modelos.nombre}
+          modelo={modelos.modelo}
+          imagen={modelos.imagen}
+          descripcion={modelos.descripcion}
+          sonido={modelos.sonido}
+          anio={modelos.anio}
+        />
+      ))}
     </div>
   );
 }
